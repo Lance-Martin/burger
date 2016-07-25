575b12648a3cf5a13e7418f1686411fc7405b206
Lance Martin <ldmartin7777@gmail.com>
2016-07-25 14:03:54 -0500

    added dependencies to package.json and commands in schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+CREATE TABLE burgers
+(
+id int NOT NULL AUTO_INCREMENT, burger_name VARCHAR(100) NOT NULL, devoured BOOLEAN NOT NULL, date TIMESTAMP NOT NULL), PRIMARY KEY (id)  
+);

a70bec1c902a38a8b943e40f22f7192973fb076d
Lance Martin <ldmartin7777@gmail.com>
2016-07-27 15:23:37 -0500

    ready to deploy with sql connection

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
 	port: 3306,
-	host: 'localhost',
-	user: 'root',
-	password: 'Zamperini77',
-	database: 'burgers_db'
+	host: 'nj5rh9gto1v5n05t.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+	user: 'o0te9snr6zch3yd2',
+	password: 'w1avzzrr6p580y77',
+	database: 'tdhpqnw4umk9qbqm'
 });
 
 connection.connect(function (err) {
